0dae4a98d0d812d98f2cb405f3d82e3fcf364288
good_pen <5649701+llwwff@user.noreply.gitee.com>
2022-10-21 11:23:24 +0800

    21日11时

--- run_test.py
+++ run_test.py
@@ -4,17 +4,17 @@ import numpy as np
 import os
 import shutil
 
-file_path = './resnet50_final_{TypE}.h5'
-test_data_dir = './test_images_{TypE}'
+TypE = 'adult'
+# TypE = 'violence'
 
+model = load_model(f'final_{TypE}.h5')
+test_data_dir = './imgs_{TypE}'
 batch_size = 1
 nb_samples = 1
 SIZE = (224, 224)
 
-model = load_model(file_path)
 
 test_datagen = ImageDataGenerator()
-
 test_generator = test_datagen.flow_from_directory(test_data_dir         ,
                                                   target_size = SIZE       ,
                                                   batch_size  = batch_size ,
